1d00b127e326a4d2b08cdafbf6db6e6774a9a1f4
puteriyudani <puteriyudani@gmail.com>
2025-02-08 22:23:49 +0700

    visitor counter

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -5,9 +5,18 @@
         <p>d'happiest Islamic pre-school</p>
         <div class="social-links">
             <a href="https://wa.me/6282268081212" class="twitter" target="_blank"><i class="bx bxl-whatsapp"></i></a>
-            <a href="https://www.facebook.com/gardenschool.gardenschool" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
-            <a href="https://www.instagram.com/gardenschool_official/" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
-            <a href="https://www.youtube.com/@gardenschool2246" class="instagram" target="_blank"><i class="bx bxl-youtube"></i></a>
+            <a href="https://www.facebook.com/gardenschool.gardenschool" class="facebook" target="_blank"><i
+                    class="bx bxl-facebook"></i></a>
+            <a href="https://www.instagram.com/gardenschool_official/" class="instagram" target="_blank"><i
+                    class="bx bxl-instagram"></i></a>
+            <a href="https://www.youtube.com/@gardenschool2246" class="instagram" target="_blank"><i
+                    class="bx bxl-youtube"></i></a>
+        </div>
+        <div class="mb-4">
+            <h6 style="font-weight: bold; color: #0563bb;">Jumlah Pengunjung:</h6>
+            <a href="https://www.freecounterstat.com" title="website counter" target="_blank"><img
+                    src="https://counter6.optistats.ovh/private/freecounterstat.php?c=9dhpx729jsnljaa7em442z73d1fzjjbs"
+                    border="0" title="website counter" alt="website counter"></a>
         </div>
         <div class="copyright">
             &copy; Copyright <strong><span>Gardenschool Indonesia</span></strong>. All Rights Reserved
